1e6f9dff7afc90729a8d119b98075c68227b6fef
Esther Edwards <Esther Edwards>
2017-07-05 12:59:55 -0500

    Prog5 Part2

--- Assignments/Program_5/geojson/load_mongo.sh
+++
@@ -1,9 +0,0 @@
-mongo rephie --eval "db.dropDatabase()"
-mongoimport --db rephie --collection airports       --type json --file airports.geojson        --jsonArray
-mongoimport --db rephie --collection countries      --type json --file countries.geojson       --jsonArray
-mongoimport --db rephie --collection meteorites     --type json --file meteorite.geojson       --jsonArray
-mongoimport --db rephie --collection volcanos       --type json --file volcanos.geojson        --jsonArray
-mongoimport --db rephie --collection earthquakes    --type json --file earthquakes.geojson     --jsonArray
-mongoimport --db rephie --collection cities         --type json --file world_cities.geojson    --jsonArray
-mongoimport --db rephie --collection states         --type json --file state_borders.geojson   --jsonArray
-mongoimport --db rephie --collection terrorism      --type json --file globalterrorism.geojson --jsonArray
